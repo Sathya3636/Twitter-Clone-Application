911cc5ea6fdff756057299a070f08cdc0ae6a99d
Sathya3636 <42291575+Sathya3636@users.noreply.github.com>
2018-08-19 10:51:18 +0530

    Profile update changes

--- BusinessLayer/PersonManager.cs
+++ BusinessLayer/PersonManager.cs
@@ -9,33 +9,64 @@ namespace BusinessLayer
 {
     public class PersonManager
     {
-       // private MVCApplicationEntities db = new MVCApplicationEntities();
-
-        //public IQueryable<Person> FindAll()
-        //{
-        //    return this.db.People;
-        //}
-
-        public void Save(Person person)
+        public string Save(Person person)
         {
             using (var db = new MVCApplicationEntities())
             {
-                db.People.Add(person);
-                db.SaveChanges();
-            }  
+                var upPerson = db.People.Where(a => a.User_Id.ToLower().Equals(person.User_Id.ToLower()) || a.Email.ToLower().Equals(person.Email.ToLower())).FirstOrDefault();
+                var rePerson = db.People.Where(a => a.User_Id.ToLower().Equals(person.User_Id.ToLower()) && a.Active == false && a.Email.ToLower().Equals(person.Email.ToLower())).FirstOrDefault();
+                if (upPerson == null)
+                {
+                    db.People.Add(person);
+                    db.SaveChanges();
+                }
+                else if (upPerson != null && rePerson != null)
+                {
+                    rePerson.Password = person.Password;
+                    rePerson.FullName = person.FullName;
+                    rePerson.Email = person.Email;
+                    rePerson.Active = true;
+                    db.SaveChanges();
+                }
+                else
+                {
+                    return "UserName or EmailId already registered with us";
+                }
+
+                return "success";
+            }
+        }
+
+        public string Update(Person person)
+        {
+            using (var db = new MVCApplicationEntities())
+            {
+                var upPerson = db.People.Where(a => a.User_Id.ToLower().Equals(person.User_Id.ToLower())).FirstOrDefault();
+                var updatePerson = db.People.Where(a => !a.User_Id.ToLower().Equals(person.User_Id.ToLower()) && a.Email.ToLower().Equals(person.Email.ToLower())).FirstOrDefault();
+                if (upPerson != null && updatePerson == null)
+                {
+                    upPerson.Password = person.Password ?? upPerson.Password;
+                    upPerson.FullName = person.FullName;
+                    upPerson.Email = person.Email;
+                    upPerson.Active = person.Active;
+                    db.SaveChanges();
+                }
+                else
+                {
+                    return "EmailId already registered with other user";
+                }
+
+                return "success";
+            }
         }
 
         public Person Login(string userName, string password)
         {
             using (var db = new MVCApplicationEntities())
             {
-                var person = db.People.Where(a => a.User_Id.Equals(userName) && a.Password.Equals(password)).FirstOrDefault();
+                var person = db.People.Where(a => a.User_Id.ToLower().Equals(userName.ToLower()) && a.Password.Equals(password) && a.Active == true).FirstOrDefault();
                 return person;
             }
         }
-        //public void Dispose()
-        //{
-        //    this.db.Dispose();
-        //}
     }
 }
--- TwitterClone/Controllers/AccountController.cs
+++ TwitterClone/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using DataAccessLayer;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
 using System;
+using System.Collections.Generic;
 using System.Web;
 using System.Web.Mvc;
 using TwitterClone.Models;
@@ -33,11 +34,23 @@ namespace TwitterClone.Controllers
         {
             if (ModelState.IsValid)
             {
-                var person = this.personManager.Login(objUser.UserName, objUser.Password);
-                if (person != null)
+                try
                 {
-                    Session["UserName"] = person.User_Id.ToString();
-                    return RedirectToAction("Tweet", "Twitter");
+                    var person = this.personManager.Login(objUser.UserName, objUser.Password);
+                    if (person != null)
+                    {
+                        Session["LoginModel"] = person;
+                        Session["UserName"] = person.User_Id.ToString();
+                        return RedirectToAction("Tweet", "Twitter");
+                    }
+                    else
+                    {
+                        AddErrors("UserName or Password is wrong..");
+                    }
+                }
+                catch (Exception)
+                {
+                    AddErrors("Problem with our end. Please try again later..");
                 }
             }
             return View(objUser);
@@ -71,86 +84,104 @@ namespace TwitterClone.Controllers
                 };
                 try
                 {
-                    this.personManager.Save(user);
-                    Session["UserName"] = user.User_Id.ToString();
-                    return RedirectToAction("Tweet", "Twitter");
-
+                    var successMsg = personManager.Save(user);
+                    if (successMsg == "success")
+                    {
+                        Session["LoginModel"] = user;
+                        Session["UserName"] = user.User_Id.ToString();
+                        return RedirectToAction("Tweet", "Twitter");
+                    }
+                    else
+                    {
+                        AddErrors(successMsg);
+                    }
                 }
-                catch (Exception ex)
+                catch (Exception)
                 {
-                    throw ex;
-                    // AddErrors(ex);
+                    AddErrors("Problem with our end. Please try again later..");
                 }
             }
 
             return View(model);
         }
 
+        //
+        // GET: /Account/Update
+        [AllowAnonymous]
+        public ActionResult Update()
+        {
+            UpdateViewModel model = null;
+            if (Session["LoginModel"] != null)
+            {
+                Person personModel = (Person)Session["LoginModel"];
+                model = new UpdateViewModel
+                {
+                    UserName = personModel.User_Id,
+                    Password = personModel.Password,
+                    FullName = personModel.FullName,
+                    Email = personModel.Email
+                };
+
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            return View(model);
+        }
+
+        //
+        // POST: /Account/Update
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update(UpdateViewModel model, string action)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new Person
+                {
+                    User_Id = model.UserName,
+                    Password = model.Password,
+                    FullName = model.FullName,
+                    Email = model.Email,
+                    Active = action == "Update" ? true : false
+                };
+                try
+                {
+                    var successMsg = personManager.Update(user);
+                    if (successMsg == "success")
+                    {
+                        Session["LoginModel"] = user;
+                        Session["UserName"] = user.User_Id.ToString();
+                        return RedirectToAction("Tweet", "Twitter");
+                    }
+                    else
+                    {
+                        AddErrors(successMsg);
+                    }
+
+                }
+                catch (Exception)
+                {
+                    AddErrors("Problem with our end. Please try again later..");
+                }
+            }
+
+            return View(model);
+        }
         [ValidateAntiForgeryToken]
         public ActionResult LogOff()
         {
             Session["UserName"] = null;
+            Session["LoginModel"] = null;
             return RedirectToAction("Login", "Account");
         }
 
-
-        #region Helpers
-        // Used for XSRF protection when adding external logins
-        private const string XsrfKey = "XsrfId";
-
-        private IAuthenticationManager AuthenticationManager
+        private void AddErrors(string error)
         {
-            get
-            {
-                return HttpContext.GetOwinContext().Authentication;
-            }
+            ModelState.AddModelError("", error);
         }
-
-        private void AddErrors(IdentityResult result)
-        {
-            foreach (var error in result.Errors)
-            {
-                ModelState.AddModelError("", error);
-            }
-        }
-
-        private ActionResult RedirectToLocal(string returnUrl)
-        {
-            if (Url.IsLocalUrl(returnUrl))
-            {
-                return Redirect(returnUrl);
-            }
-            return RedirectToAction("Index", "Home");
-        }
-
-        internal class ChallengeResult : HttpUnauthorizedResult
-        {
-            public ChallengeResult(string provider, string redirectUri)
-                : this(provider, redirectUri, null)
-            {
-            }
-
-            public ChallengeResult(string provider, string redirectUri, string userId)
-            {
-                LoginProvider = provider;
-                RedirectUri = redirectUri;
-                UserId = userId;
-            }
-
-            public string LoginProvider { get; set; }
-            public string RedirectUri { get; set; }
-            public string UserId { get; set; }
-
-            public override void ExecuteResult(ControllerContext context)
-            {
-                var properties = new AuthenticationProperties { RedirectUri = RedirectUri };
-                if (UserId != null)
-                {
-                    properties.Dictionary[XsrfKey] = UserId;
-                }
-                context.HttpContext.GetOwinContext().Authentication.Challenge(properties, LoginProvider);
-            }
-        }
-        #endregion
     }
 }
